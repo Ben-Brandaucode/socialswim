307bab793fedaaf205ce72f700e308677e6407f5
Ben Brandau <codingbrrandau@gmail.com>
2021-08-02 16:19:03 -0400

    socialSetup

---
+++ socialswim/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import SocialInput from './components/maininterface';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>the main interface should be below</h1>
+     <SocialInput/>
+    </div>
+  );
+}
+
+export default App;
---
+++ socialswim/src/components/maininterface.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function SocialInput(){
+    return
+    <h1>ask a Question here</h1>;
+
+}
+export default SocialInput;
